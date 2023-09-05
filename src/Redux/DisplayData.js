ce51314da3aa38e68c2d03cc986299ac6a16d17f
Akanksha <akanksharma0829@gmail.com>
2023-09-05 13:55:49 +0530

    again commit

--- src/Redux/DisplayData.js
+++ src/Redux/DisplayData.js
@@ -1,13 +1,23 @@
-import {createSlice } from "@reduxjs/toolkit";
-const initialState=0;
-const CounterSlice= createSlice({
-    name:"counter",
-    initialState,
-    reducers:{
-        add:(state,{payload})=>state+=payload,
-        sub:(state,{payload})=>state-=payload
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Sub, add } from './CounterSlice';
 
+function DisplayData() {
+    const dispatch = useDispatch();
+    const {count} = useSelector(state => state)
+    function handleAdd(){
+        dispatch(add(5))
     }
-})
-export const{add,sub}=CounterSlice.actions
-export default CounterSlice.reducer
+    function handleSub(){
+        dispatch(Sub(1))
+    }
+  return (
+    <>
+    <h1>{count}</h1>
+     <button onClick={handleAdd}>Increment</button> 
+     <button onClick={handleSub}>Decrement</button> 
+    </>
+  )
+}
+
+export default DisplayData
